23d9906829ab2c30c2c6eecab709e7e9faf0103f
Julio Ruiz Flores <julio.ruiz@softtek.com>
2016-10-18 17:06:46 -0500

    Completing Chapter 4

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -28,11 +28,58 @@ var homelist = function(req, res) {
 };
 
 var locationInfo = function(req, res) {
-    res.render('location-info', {title: 'Location Info'});
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with you laptop and get some work done',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 SP1',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat: 21.928147,
+                lng: -102.337510
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Julio Ruiz Flores',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great but the wifi was fast'
+            }]
+        }
+    });
 };
 
 var addReview = function(req, res) {
-    res.render('location-review-form', {title: 'Add Review'});
+    res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: {title: 'Review Starcups' }
+    });
 };
 
 module.exports = {
