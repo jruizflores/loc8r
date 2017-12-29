61f4ed098e482c60db4912cd8df7f7829b971709
Julio César Ruiz Flores Rangel <jc@Air-de-Julio.lan>
2017-12-29 13:43:42 -0600

    At page 247, still need to fix reviews.js

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,4 +1,10 @@
-var express = require('express');
+var mongoose = require('mongoose');
+var Loc8r = mongoose.model('Location');
+
+function sendJsonResponse(res, code, obj) {
+    res.status(code);
+    res.json(obj);
+}
 
 module.exports.reviewsCreate = function(req, res) {
     res.status(200);
@@ -6,8 +12,15 @@ module.exports.reviewsCreate = function(req, res) {
 }
 
 module.exports.reviewsReadOne = function(req, res) {
-    res.status(200);
-    res.json({"status": "success"});
+    if(req.params && req.params.locationid && req.params.reviewid) {
+        Loc8r.findById(req.params.locationid)
+                .select('name reviews')
+                .exec(function(err, location) {
+                    if(!location) {
+                        
+                    }
+        })
+    }
 }
 
 module.exports.reviewsUpdateOne = function(req, res) {
